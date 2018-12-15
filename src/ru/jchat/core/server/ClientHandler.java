0da2938c72c65abacfc708cb45f19246d529b9a9
d.kudryashov <d.kudryashov2014@yandex.ru>
2018-12-15 19:05:54 +0500

    Homework for lesson 8

--- src/ru/jchat/core/server/ClientHandler.java
+++ src/ru/jchat/core/server/ClientHandler.java
@@ -7,6 +7,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.*;
 
 public class ClientHandler {
     private Server server;
@@ -14,6 +15,7 @@ public class ClientHandler {
     private DataInputStream in;
     private DataOutputStream out;
     private String nick;
+    private boolean isLoggedIn;
 
     public String getNick() {
         return nick;
@@ -29,14 +31,15 @@ public class ClientHandler {
                 try{
                     while(true){
                         String msg = in.readUTF();
-                        if (msg.startsWith("/auth ")){
-                            String[] data = msg.split("\\s");
+                        String[] data = msg.split("\\s");
+                        if (msg.startsWith("/auth ") && data.length > 2){
                             String newNick = server.getAuthService().getNickByLoginAndPass(data[1], data[2]);
                             if (newNick != null){
                                 if (!server.isNickBusy(newNick)){
                                     nick = newNick;
                                     sendMsg("/authok");
                                     server.subscribe(this);
+                                    isLoggedIn = true;
                                     break;
                                 } else {
                                     sendMsg("Учетная запись уже занята");
@@ -67,6 +70,20 @@ public class ClientHandler {
                     }
                 }
 
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    Thread.sleep(120000);
+                    if (!isLoggedIn) {
+                        socket.close();
+                        System.out.println("Socket closed.");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }).start();
 
         } catch (IOException e) {
